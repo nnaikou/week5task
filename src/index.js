0e740b238e1a9f6c1075aec228798c540e14d26f
NikoS <122801074+nnaikou@users.noreply.github.com>
2023-07-06 14:29:25 +0000

    Update 7 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,69 @@
 import "./styles.css";
+import "./leaflet.css";
+import L from "./leaflet";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+/*let xVar = 0;*/
+
+const fetchData = async () => {
+  const url =
+    "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
+  const res = await fetch(url);
+  const data = await res.json();
+
+  initMap(data);
+};
+
+const initMap = (data) => {
+  let map = L.map("map", {
+    minZoom: -3,
+  });
+
+  let geoJson = L.geoJSON(data, {
+    weight: 2,
+    onEachFeature: getFeature,
+  }).addTo(map);
+
+  let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap",
+  }).addTo(map);
+
+  map.fitBounds(geoJson.getBounds());
+};
+
+const getFeature = async (feature, layer) => {
+  const name = feature.properties.name;
+  layer.bindTooltip(name);
+
+  /*const negOneValue = giveNegData();
+  const posOneValue = givePosData();
+  layer.bindPopup(
+    `<ul>
+      <li>Positive mig: ${posOneValue}</li>
+      <li>Negative mig: ${negOneValue}</li>
+    </ul>`
+  );
+
+  xVar++;*/
+
+  /*console.log(feature.properties.name)*/
+};
+
+/*async function giveNegData() {
+  const url1 =
+    "https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f";
+  const res1 = await fetch(url1);
+  const negData = await res1.json();
+  const values = Object.values(negData.dataset.value);
+  return values[xVar];
+}
+
+async function givePosData() {
+  const url2 =
+    "https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e";
+  const res2 = await fetch(url2);
+  const posData = await res2.json();
+  const values = Object.values(posData.dataset.value);
+  return values[xVar];
+}*/
+
+fetchData();
